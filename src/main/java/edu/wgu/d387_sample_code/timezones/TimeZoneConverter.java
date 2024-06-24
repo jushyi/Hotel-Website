cf9ab6d8b8637e625a9869f4858cce011f01c1f2
Josh <jmaser3@wgu.edu>
2024-06-24 13:45:10 -0400

    B3. A Wrote a Java method to convert times between eastern time (ET), mountain time (MT), and coordinated universal time (UTC) zones.

---
+++ src/main/java/edu/wgu/d387_sample_code/timezones/TimeZoneConverter.java
@@ -0,0 +1,31 @@
+package edu.wgu.d387_sample_code.timezones;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public class TimeZoneConverter {
+
+    public static String getTime(){
+
+        ZonedDateTime time = ZonedDateTime.now();
+        //B3. b. requires hours and minutes to be displayed. Using DateTimeFormatter to format the time.
+        DateTimeFormatter displayFormat = DateTimeFormatter.ofPattern("HH:mm");
+        ZoneId zEastern = ZoneId.of("America/New_York");
+        ZoneId zPacific = ZoneId.of("America/Los_Angeles");
+        ZoneId zUTC = ZoneId.of("UTC");
+
+        ZonedDateTime ET = time.withZoneSameInstant(zEastern);
+        ZonedDateTime MT = time.withZoneSameInstant(zPacific);
+        ZonedDateTime UTC = time.withZoneSameInstant(zUTC);
+
+        return ET.format(displayFormat) + "ET, "
+                + MT.format(displayFormat) + "MT, "
+                + UTC.format(displayFormat) + "UTC";
+
+    }
+
+}
